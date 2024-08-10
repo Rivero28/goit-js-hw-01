3a7c80e6510ecd9586c15d84e31f952194bfdefb
Oliwia <obrycka.oliwia@wp.pl>
2024-08-10 23:56:33 +0200

    2 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
 function makeTransaction(quantity, pricePerDroid) {
-  console.log("You ordered ${quantity} droids worth ${totalPrice} credits!");
+  const totalPrice = pricePerDroid * quantity;
+  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
 }
-let totalPrice = pricerPerDroid * quantity;
